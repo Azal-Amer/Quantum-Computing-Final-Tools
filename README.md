cac0bfbef6e36baeeb2eeaabeb6783c18f1694c3
Azal-Amer <azalamer6@gmail.com>
2024-05-05 20:45:53 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,76 @@
-# Quantum Computing Class Tools
- A set of objects used for performing math for simple quantum computing behaviors, with an example written
+# Quantum Photon Simulation
+This project provides a Python implementation for simulating quantum photons and applying various quantum gates. It utilizes the SymPy library for symbolic mathematics and the IPython display module for visualization.
+## Prerequisites
+To run this project, you need to have the following dependencies installed:
+
+Python (version 3.x)
+SymPy
+NumPy
+IPython
+
+You can install the required packages using pip:
+``` pip install sympy numpy ipython```
+## Usage
+
+1. Import the necessary classes and functions from the main Python script:
+```from quantum_photon_simulation import Photon, beamSplitter, halfWavePlate```
+2. Create a Photon instance with the desired polarization, amplitudes, and direction:
+``` photon = Photon(polarization='HV', amplitudes=sp.Matrix([1, 0]), direction='-')```
+3. Apply quantum gates to the photon using the * operator:
+```
+photon = photon * X  # Apply Pauli-X gate
+photon = photon * H  # Apply Hadamard gate
+```
+4. Convert the photon to different polarization bases using the convertHV(), convertDA(), or convertRL() methods:
+```
+digital_hv = photon.convertHV()
+digital_da = photon.convertDA()
+digital_rl = photon.convertRL()
+```
+5. Visualize the wavefunction  using the visualize() method:
+```photon.visualize()```
+This will display the photon state in LaTeX format using IPython.
+
+6. Apply a beam splitter to the photon using the beamSplitter class:
+```
+beam_splitter = beamSplitter(r=0.5)
+output_photons = beam_splitter.apply(photon)
+```
+The apply() method returns a list of two photons representing the horizontal and vertical components after passing through the beam splitter.
+7. Apply a half-wave plate to the photon using the halfWavePlate class:
+```
+ half_wave_plate = halfWavePlate(theta=sp.pi/4)
+ output_photon = half_wave_plate.apply(photon)
+ ```
+The apply() method returns a new photon with the amplitudes rotated by the specified angle.
+Quantum Gates
+The following quantum gates are predefined in the script:
+
+- Identity gate (I)
+- Pauli-X gate (X)
+- S gate (S)
+- Pauli-Y gate (Y)
+- T gate (T)
+- Hadamard gate (H)
+- Projection gate (P_0)
+- Pauli-Z gate (Z)
+- S_dagger gate (S_dagger)
+
+You can apply these gates to a photon using the * operator.
+## Example
+Here's an example that demonstrates the usage of the Photon class and the application of quantum gates for the following scenario:
+![Example problem with a beamsplitter and a photon](example.png)
+
+```
+incomingPhoton = Photon(polarization='HV', amplitudes=sp.Matrix([1,0]), direction='-')
+newBeamSplitter = beamSplitter(A = sp.Rational(1,2),B = sp.Rational(1,2))
+newHalfWavePlate = halfWavePlate(sp.pi)
+outputPhotons = newBeamSplitter.apply(incomingPhoton)
+# running the photon through a beamsplitter, the output is [horizontal, vertical]
+horizontalPhoton = outputPhotons[0]
+verticalPhoton = newHalfWavePlate.apply(outputPhotons[1])*X
+verticalPhoton.visualize(), horizontalPhoton.visualize()
+```
+The above code then returns the below output:
+![Example problem output](exampleOutput.png)
+
---
+++ quantumObjects.py
@@ -0,0 +1,113 @@
+import numpy as np
+import sympy as sp
+from IPython.display import display, Math
+
+# Identity gate (I)
+I = sp.Matrix([[1, 0], [0, 1]])
+
+# Pauli-X gate (X)
+X = sp.Matrix([[0, 1], [1, 0]])
+
+# Phase gate (S)
+S = sp.Matrix([[1, 0], [0, sp.I]])
+
+# Pauli-Y gate (Y)
+Y = sp.Matrix([[0, -sp.I], [sp.I, 0]])
+
+# T gate (T)
+T = sp.Matrix([[1, 0], [0, sp.exp(sp.I * sp.pi / 4)]])
+
+# Hadamard gate (H)
+H = sp.Matrix([[1, 1], [1, -1]]) / sp.sqrt(2)
+
+# Projection gate (P_0)
+P_0 = sp.Matrix([[1, 0], [0, 0]])
+
+# Pauli-Z gate (Z)
+Z = sp.Matrix([[1, 0], [0, -1]])
+
+# Conjugate transpose of the Phase gate (S^dagger)
+S_dagger = sp.Matrix([[1, 0], [0, -sp.I]])
+class Photon():
+    H = sp.Rational(1)/sp.sqrt(2)*sp.Matrix([[1,1],[1,-1]])
+    Sdag = sp.Matrix([[1,0],[0,-sp.I]])
+    S = sp.Matrix([[1,0],[0,sp.I]])
+    def __init__(self, polarization = 'HV', amplitudes = sp.Matrix([1,0]),direction = None):
+        self.polarization = polarization
+        self.amplitudes = amplitudes
+        self.direction = direction
+        if(self.polarization=='HV'):
+            self.digital = self.amplitudes
+        elif(self.polarization=='DA'):
+            self.digital = self.H*self.amplitudes
+        elif(self.polarization=='RL'):
+            self.digital = self.S*self.H*self.amplitudes
+    def convertHV(self):
+        return self.digital
+    def convertDA(self):
+        return self.H*self.digital
+    def convertRL(self):
+        return self.S*self.H*self.digital
+    def __mul__(self,gate):
+        return Photon(self.polarization,gate*self.amplitudes,self.direction)
+    def _repr_mimebundle_(self, **kwargs):
+        direction_symbol = ''
+        if(self.direction == '|'):
+            direction_symbol = '\\downarrow ,'
+        elif(self.direction == '-'):
+            direction_symbol = '\\rightarrow ,'
+        amplitude_0 = sp.simplify(self.digital[0])
+        amplitude_1 = sp.simplify(self.digital[1])
+
+        latex_code = f"|{direction_symbol} \\psi \\rangle = {sp.latex(amplitude_0)}|0\\rangle + {sp.latex(amplitude_1)}|1\\rangle"
+        display(Math(latex_code))
+    def visualize(self):
+        direction_symbol = ''
+        if(self.direction == '|'):
+            direction_symbol = '\\downarrow ,'
+        elif(self.direction == '-'):
+            direction_symbol = '\\rightarrow ,'
+        amplitude_0 = sp.simplify(self.digital[0])
+        amplitude_1 = sp.simplify(self.digital[1])
+
+        latex_code = f"|{direction_symbol} \\psi \\rangle = {sp.latex(amplitude_0)}|0\\rangle + {sp.latex(amplitude_1)}|1\\rangle"
+        
+        display(Math(latex_code))
+    # add all the regular gates as things we can access, let's make the directional photon stuff
+class beamSplitter():
+    def __init__(self, A = None,B= None,r = None):
+
+        self.A = A
+        self.B = B
+
+        if(r is None):
+            self.r = sp.sqrt(B)
+        else:
+            self.r = r
+    def apply(self, photon):
+        if(photon.direction is None):
+            print('No direction specified')
+            return
+        else:
+            if(photon.direction == '-'):
+                horzAmp = sp.sqrt(1-self.r**2)
+                vertAmp = self.r
+            elif(photon.direction == '|'):
+                horzAmp = self.r
+                vertAmp = -sp.sqrt(1-self.r**2)
+                # then we wanna make two new photons, with the previous two photons amplitudes scaled by these new factors
+            returnedHorzPhoton = Photon(photon.polarization, horzAmp*photon.amplitudes, '-')
+            returnedVertPhoton = Photon(photon.polarization, vertAmp*photon.amplitudes, '|')
+            return [returnedHorzPhoton, returnedVertPhoton]
+# the beamsplitter class takes some input probabilities, or a value of r, and will return two photons depending
+# on the direction of the incoming photon
+class halfWavePlate():
+    def __init__(self,theta):
+        self.theta = theta
+    def apply(self,photon):
+        if(photon.direction is None):
+            print('No direction specified')
+            return
+        else:
+            return Photon(photon.polarization, sp.exp(sp.I*self.theta)*photon.amplitudes, photon.direction)
+# the half wave plate class takes an angle, and will return a photon with the amplitudes rotated by that angle
